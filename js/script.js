f968928ca9449647134e1089b3daf9e1df7db5b9
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-07-14 09:21:44 +0100

    fix: host images online to reduce project size

--- js/script.js
+++ js/script.js
@@ -20,204 +20,205 @@ const filters = [
     "waterfalls",
     "wet leaves",
 ];
+
 const gallery = {
     img1: {
-        src: "../images/autumn/pexels-eberhard-grossgasteiger-2310641.jpg",
+        src: "https://thumbs2.imgbox.com/42/c7/pkaelCSi_t.jpg",
         category: "autumn",
         filters: ["trees"],
     },
     img2: {
-        src: "../images/autumn/pexels-irina-iriser-1590549.jpg",
+        src: "https://thumbs2.imgbox.com/03/12/Lf5Nth86_t.jpg",
         category: "autumn",
         filters: ["leaves", "sun"],
     },
     img3: {
-        src: "..images/autumn/pexels-jara-3083250.jpg",
+        src: "https://thumbs2.imgbox.com/d8/4d/cIoNqnBJ_t.jpg",
         category: "autumn",
         filters: ["leaves", "dew", "water", "wet leaves"],
     },
     img4: {
-        src: "../images/autumn/pexels-josh-hild-14269090.jpg",
+        src: "https://thumbs2.imgbox.com/b2/f2/J4Lnnwxs_t.jpg",
         category: "autumn",
         filters: ["animal", "grass"],
     },
     img5: {
-        src: "../images/autumn/pexels-lil-artsy-3127729.jpg",
+        src: "https://thumbs2.imgbox.com/b5/18/MQxwJ979_t.jpg",
         category: "autumn",
         filters: ["animal", "leaves", "dry leaves"],
     },
     img6: {
-        src: "../images/autumn/pexels-lisa-fotios-2812164.jpg",
+        src: "https://thumbs2.imgbox.com/45/d5/O6Ak8lLo_t.jpg",
         category: "autumn",
         filters: ["forest", "trees"],
     },
     img7: {
-        src: "../images/autumn/pexels-luke-barky-2899722.jpg",
+        src: "https://thumbs2.imgbox.com/98/6b/kW1hagTn_t.jpg",
         category: "autumn",
         filters: ["sunset", "grass"],
     },
     img8: {
-        src: "../images/autumn/pexels-maroš-markovič-1468735.jpg",
+        src: "https://thumbs2.imgbox.com/48/e8/HVTjEAQ7_t.jpg",
         category: "autumn",
         filters: ["stream", "rocks", "wet leaves"],
     },
     img9: {
-        src: "../images/autumn/pexels-petar-starčević-2389122.jpg",
+        src: "https://thumbs2.imgbox.com/54/04/VgR086Qg_t.jpg",
         category: "autumn",
         filters: ["field", "grass"],
     },
     img10: {
-        src: "../images/autumn/pexels-ylanite-koppens-612816.jpg",
+        src: "https://thumbs2.imgbox.com/88/c1/lNiJfVzy_t.jpg",
         category: "autumn",
         filters: ["leaves"],
     },
     img11: {
-        src: "..images/spring/pexels-afr-creation-15994490.jpg",
+        src: "https://thumbs2.imgbox.com/2d/fb/jBZgzuX5_t.jpg",
         category: "spring",
         filters: ["water", "leaves", "wet leaves"],
     },
     img12: {
-        src: "..images/spring/pexels-andreas-15985749.jpg",
+        src: "https://thumbs2.imgbox.com/24/83/9qapZXQ9_t.jpg",
         category: "spring",
         filters: ["trees", "grass", "field"],
     },
     img13: {
-        src: "..images/spring/pexels-david-brown-16001827.jpg",
+        src: "https://thumbs2.imgbox.com/a2/3a/WSkUck3X_t.jpg",
         category: "spring",
         filters: ["water", "animal"],
     },
     img14: {
-        src: "../images/spring/pexels-denniz-futalan-5049271.jpg",
+        src: "https://thumbs2.imgbox.com/bf/8c/F13sxOP1_t.jpg",
         category: "spring",
         filters: ["animal", "grass", "leaves"],
     },
     img15: {
-        src: "../images/spring/pexels-dids-4261738.jpg",
+        src: "https://thumbs2.imgbox.com/ed/63/8VDl40VI_t.jpg",
         category: "spring",
         filters: ["water", "leaves", "wet leaves"],
     },
     img16: {
-        src: "../images/spring/pexels-jonathan-petersson-1076183.jpg",
+        src: "https://thumbs2.imgbox.com/aa/5e/mwSUiWvk_t.jpg",
         category: "spring",
         filters: ["trees", "grass"],
     },
     img17: {
-        src: "../images/spring/pexels-maria-orlova-4906295.jpg",
+        src: "https://thumbs2.imgbox.com/bd/c4/Fg7DyfeC_t.jpg",
         category: "spring",
         filters: ["leaves", "flowers"],
     },
     img18: {
-        src: "..images/spring/pexels-pixabay-40731.jpg",
+        src: "https://thumbs2.imgbox.com/bf/ce/KlsXgaEU_t.jpg",
         category: "spring",
         filters: ["grass", "water", "wet leaves", "animal"],
     },
     img19: {
-        src: "../images/spring/pexels-pixabay-86431.jpg",
+        src: "https://thumbs2.imgbox.com/c5/d2/zZ23cky6_t.jpg",
         category: "spring",
         filters: ["flowers", "field", "grass", "clouds"],
     },
     img20: {
-        src: "../images/spring/pexels-q-hưng-phạm-15962267.jpg",
+        src: "https://thumbs2.imgbox.com/8f/1d/WBwzYVSo_t.jpg",
         category: "spring",
         filters: ["trees", "flowers"],
     },
     img21: {
-        src: "../images/summer/pexels-asad-photo-maldives-3601421.jpg",
+        src: "https://thumbs2.imgbox.com/f3/f8/rRLdW4v8_t.jpg",
         category: "summer",
         filters: ["sunset", "water", "sea"],
     },
     img22: {
-        src: "../images/summer/pexels-dom-gould-325807.jpg",
+        src: "https://thumbs2.imgbox.com/0b/97/uNXYiQ84_t.jpg",
         category: "summer",
         filters: ["trees", "rocks", "water"],
     },
     img23: {
-        src: "../images/summer/pexels-james-wheeler-1486974.jpg",
+        src: "https://thumbs2.imgbox.com/47/3e/Wi7BeJa4_t.jpg",
         category: "summer",
         filters: ["trees", "grass", "water", "clouds"],
     },
     img24: {
-        src: "../images/summer/pexels-partha-chatterjee-16070595.jpg",
+        src: "https://thumbs2.imgbox.com/b2/65/WdPnK2Mp_t.jpg",
         category: "summer",
         filters: ["flowers"],
     },
     img25: {
-        src: "../images/summer/pexels-pixabay-262713.jpg",
+        src: "https://thumbs2.imgbox.com/55/08/sw6CAU1Y_t.jpg",
         category: "summer",
         filters: ["flowers", "field"],
     },
     img26: {
-        src: "../images/summer/pexels-pixabay-302549.jpg",
+        src: "https://thumbs2.imgbox.com/71/84/oBU0TLVr_t.jpg",
         category: "summer",
         filters: ["rocks", "trees", "water", "ocean"],
     },
     img27: {
-        src: "../images/summer/pexels-pixabay-358457.jpg",
+        src: "https://thumbs2.imgbox.com/18/e9/ZID1NOjO_t.jpg",
         category: "summer",
         filters: ["waterfalls", "rocks", "trees"],
     },
     img28: {
-        src: "../images/summer/pexels-robert-so-15968083.jpg",
+        src: "https://thumbs2.imgbox.com/ef/27/qL69wJTN_t.jpg",
         category: "summer",
         filters: ["field", "grass", "trees", "rocks"],
     },
     img29: {
-        src: "../images/summer/pexels-roberto-nickson-2631613.jpg",
+        src: "https://thumbs2.imgbox.com/0f/0b/0kxlT2Ps_t.jpg",
         category: "summer",
         filters: ["beach", "trees", "ocean", "water"],
     },
     img30: {
-        src: "../images/summer/pexels-ryan-delfin-2270389.jpg",
+        src: "https://thumbs2.imgbox.com/66/eb/Xr0ODXJ3_t.jpg",
         category: "summer",
         filters: ["clouds", "trees"],
     },
     img31: {
-        src: "../images/winter/pexels-alissa-nabiullina-997567.jpg",
+        src: "https://thumbs2.imgbox.com/d5/24/aU2zqvcc_t.jpg",
         category: "winter",
         filters: ["snow", "flowers"],
     },
     img32: {
-        src: "../images/winter/pexels-annika-thierfeld-735986.jpg",
+        src: "https://thumbs2.imgbox.com/43/02/iiA3DBHH_t.jpg",
         category: "winter",
         filters: ["snow", "animal", "trees"],
     },
     img33: {
-        src: "../images/winter/pexels-eberhard-grossgasteiger-1366919.jpg",
+        src: "https://thumbs2.imgbox.com/ce/7b/KACwfss9_t.jpg",
         category: "winter",
         filters: ["snow", "rocks"],
     },
     img34: {
-        src: "../images/winter/pexels-eberhard-grossgasteiger-1417651.jpg",
+        src: "https://thumbs2.imgbox.com/b0/46/tY2S6pnx_t.jpg",
         category: "winter",
         filters: ["field", "rocks", "snow"],
     },
     img35: {
-        src: "../images/winter/pexels-egor-kamelev-813872.jpg",
+        src: "https://thumbs2.imgbox.com/7b/68/5M3aSssl_t.jpg",
         category: "winter",
         filters: ["ice", "flowers", "grass"],
     },
     img36: {
-        src: "../images/winter/pexels-julia-volk-5111048.jpg",
+        src: "https://thumbs2.imgbox.com/ca/af/bkbQyFBK_t.jpg",
         category: "winter",
         filters: ["ice", "snow", "water", "rocks"],
     },
     img37: {
-        src: "../images/winter/pexels-nikolai-ulltang-325139.jpg",
+        src: "https://thumbs2.imgbox.com/d8/67/AThk6k8q_t.jpg",
         category: "winter",
         filters: ["trees", "water"],
     },
     img38: {
-        src: "../images/winter/pexels-pixabay-86405.jpg",
+        src: "https://thumbs2.imgbox.com/99/86/kb4j2n3w_t.jpg",
         category: "winter",
         filters: ["ice", "snow", "animal"],
     },
     img39: {
-        src: "../images/winter/pexels-rok-romih-3894112.jpg",
+        src: "https://thumbs2.imgbox.com/2c/ef/jTGJL6Qz_t.jpg",
         category: "winter",
         filters: ["animal", "snow"],
     },
     img40: {
-        src: "../images/winter/pexels-simon-berger-3732527.jpg",
+        src: "https://thumbs2.imgbox.com/3e/db/yN3PTTC7_t.jpg",
         category: "winter",
         filters: ["trees", "snow"],
     },
@@ -274,16 +275,3 @@ const showSlides = () => {
 showSlides(slideIndex);
 
 /*************************************** FILTERS **************************************/
-
-// const sectionFilters = document.querySelector(".filters");
-// for (let i = 0; i < filters.length; i++) {
-//     let btnContainer = document.createElement("div");
-//     btnContainer.className = "filters__btn-container";
-
-//     let btnElem = document.createElement("button");
-//     btnElem.className = "filters__btn";
-//     btnElem.innerHTML = `${filters[i]}`;
-
-//     btnContainer.append(btnElem);
-//     sectionFilters.append(btnContainer);
-// }
