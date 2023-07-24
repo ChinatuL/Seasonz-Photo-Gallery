641615a1e9e65fc094587810dce454b211a2b3e1
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-07-24 18:54:41 +0100

    fix: make images load on scroll when filtered or by category

--- js/script.js
+++ js/script.js
@@ -3,18 +3,7 @@ import { filters, gallery } from "./store.js";
 /******************************************* NAVBAR ********************************************/
 const navBtn = document.getElementById("navbar__btn");
 const navLinks = document.querySelectorAll(".nav__link");
-const navbar = document.querySelector(".nav");
-const navbarHeight = navbar.getBoundingClientRect().height;
-
-navBtn.addEventListener("click", toggleMenu);
-navLinks.forEach((link) => link.addEventListener("click", handleActiveNav));
-window.addEventListener("scroll", (e) => {
-    if (e.currentTarget.scrollY > navbarHeight) {
-        navbar.classList.add("white-nav");
-    } else {
-        navbar.classList.remove("white-nav");
-    }
-});
+const navLogo = document.getElementById("home");
 
 function toggleMenu() {
     const navHarmburger = document.getElementById("nav__harmburger");
@@ -34,12 +23,37 @@ function toggleMenu() {
         });
     }
 }
+navBtn.addEventListener("click", toggleMenu);
 
 function handleActiveNav(e) {
     navLinks.forEach((link) => link.classList.remove("active"));
-    e.target.classList.add("active");
+    e.classList.add("active");
 }
 
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        handleActiveNav(link);
+
+        if (link.classList.contains("active")) {
+            displayImagesByCategory(link.innerText.toLowerCase());
+        }
+    });
+});
+
+navLogo.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+});
+
+window.addEventListener("scroll", (e) => {
+    const navbar = document.querySelector(".nav");
+    const navbarHeight = navbar.getBoundingClientRect().height;
+
+    if (e.currentTarget.scrollY > navbarHeight) {
+        navbar.classList.add("white-nav");
+    } else {
+        navbar.classList.remove("white-nav");
+    }
+});
 /******************************************* NAVBAR ********************************************/
 
 /******************************************* HERO SECTION ********************************************/
@@ -58,53 +72,6 @@ const showSlides = () => {
 };
 showSlides(slideIndex);
 /******************************************* HERO SECTION ********************************************/
-
-/******************************************* RENDER IMAGES ********************************************/
-
-const galleryContainer = document.querySelector(".gallery");
-const galleryHeading = document.querySelector(".gallery__subheading");
-const galleryClone = gallery.slice();
-
-let visibleImages = 0;
-
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.1,
-};
-
-const observer = new IntersectionObserver(renderImage, options);
-observer.observe(galleryContainer);
-
-function shuffleGallery(gallery) {
-    for (let i = gallery.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [gallery[i], gallery[j]] = [gallery[j], gallery[i]];
-    }
-}
-
-function renderImage(enteries, observer) {
-    shuffleGallery(galleryClone);
-    enteries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const imagesToLoad = 6;
-
-            for (let i = visibleImages; i < visibleImages + imagesToLoad; i++) {
-                if (i >= galleryClone.length) {
-                    observer.disconnect();
-                    return;
-                }
-                const imageContainer = document.createElement("div");
-                imageContainer.setAttribute("class", "gallery__image");
-                imageContainer.innerHTML = `<img src="${galleryClone[i].src}" alt="${galleryClone[i].alt}"/>`;
-                galleryContainer.append(imageContainer);
-            }
-            visibleImages += imagesToLoad;
-        }
-    });
-}
-
-/******************************************* RENDER IMAGES ********************************************/
 
 /******************************************* FILTERS ********************************************/
 const inputElem = document.querySelector(".filters__form-input");
@@ -126,43 +93,37 @@ inputElem.addEventListener("keyup", () => {
                 .toLowerCase()
                 .startsWith(inputValue.toLowerCase().trim());
         });
+
         displaySuggestions(inputValue);
     }
-    selectFilter(inputElem);
-});
 
-const form = document.querySelector(".filters__form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (inputElem.value.trim() && filters.includes(inputElem.value.trim())) {
-        setRecents(inputElem.value.trim());
-        inputElem.value = "";
-        return true;
-    }
+    selectFilter(inputElem);
 });
 
 function displaySuggestions(input) {
     suggestions.map((word) => {
         let listItem = document.createElement("li");
         listItem.setAttribute("class", "filters__item");
+
         let match =
             "<b>" +
             word.substring(0, input.trim().length) +
             "</b>" +
             word.substring(input.trim().length);
         listItem.innerHTML = match;
+
         filtersElem.append(listItem);
         filtersElem.classList.add("show");
     });
 }
 
 function clearSuggestions() {
-    let suggestions = document.querySelectorAll(".filters__item");
-    suggestions.forEach((suggestion) => suggestion.remove());
+    filtersElem.innerHTML = "";
 }
 
 function selectFilter(input) {
     let suggestions = document.querySelectorAll(".filters__item");
+
     suggestions.forEach((suggestion) =>
         suggestion.addEventListener("click", (e) => {
             input.value = e.target.innerText;
@@ -177,20 +138,116 @@ function setRecents(filter) {
         recentFilters.delete([...recentFilters][0]);
     }
     recentFilters.add(filter);
+
     localStorage.setItem("recentFilters", JSON.stringify([...recentFilters]));
+
     let savedFilters = JSON.parse(localStorage.getItem("recentFilters"));
     let recentFilter = document.createElement("li");
     recentFilter.textContent = savedFilters.slice(-1)[0];
     recentFiltersElem.append(recentFilter);
+
     if (recentFiltersElem.children) {
         recentFiltersElem.previousElementSibling.classList.add("show");
         recentFiltersElem.classList.add("show");
     }
+
     if (recentFilter.isEqualNode(recentFilter.previousElementSibling)) {
         recentFilter.remove();
     }
+
     if (recentFiltersElem.children.length === 6) {
         recentFiltersElem.firstElementChild.remove();
     }
 }
+
+const form = document.querySelector(".filters__form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (inputElem.value.trim() && filters.includes(inputElem.value.trim())) {
+        displayImagesByFilter(inputElem.value);
+        setRecents(inputElem.value.trim());
+        inputElem.value = "";
+        return true;
+    }
+});
 /******************************************* FILTERS ********************************************/
+
+/******************************************* RENDER IMAGES ********************************************/
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryHeading = document.querySelector(".gallery__subheading");
+
+function shuffleGallery(gallery) {
+    for (let i = gallery.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [gallery[i], gallery[j]] = [gallery[j], gallery[i]];
+    }
+}
+shuffleGallery(gallery);
+
+function displayImages(images) {
+    images.forEach((image) => {
+        const imageContainer = document.createElement("div");
+        imageContainer.setAttribute("class", "gallery__image");
+        imageContainer.innerHTML = `<img src="${image.src}" alt="${image.alt}"/>`;
+        galleryContainer.append(imageContainer);
+    });
+}
+
+function displayImagesByCategory(category) {
+    const filteredImages = gallery.filter(
+        (image) => image.category === category
+    );
+    displayImages(filteredImages);
+    loadImagesByArray(filteredImages);
+}
+
+function displayImagesByFilter(filter) {
+    const filteredImages = gallery.filter((image) =>
+        image.filters.includes(filter)
+    );
+    displayImages(filteredImages);
+    loadImagesByArray(filteredImages);
+}
+
+function loadMoreImages(images, index) {
+    for (let i = index; i < index + 3; i++) {
+        if (i >= images.length) {
+            window.removeEventListener("scroll", imageScroll);
+            return;
+        }
+        const imageContainer = document.createElement("div");
+        imageContainer.setAttribute("class", "gallery__image");
+        imageContainer.innerHTML = `<img src="${images[i].src}" alt="${images[i].alt}"/>`;
+
+        galleryContainer.append(imageContainer);
+    }
+}
+
+let loadedImages = 3;
+let imagesArr = [];
+function imageScroll() {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollPosition = window.scrollY;
+
+    if (windowHeight + scrollPosition >= documentHeight - 150) {
+        loadMoreImages(imagesArr, loadedImages);
+        loadedImages += 3;
+    }
+}
+
+function loadImagesByArray(images) {
+    loadedImages = 3;
+    galleryContainer.innerHTML = "";
+    imagesArr = images;
+    displayImages(images.slice(0, 3));
+    window.addEventListener("scroll", imageScroll);
+    window.scroll({
+        top: galleryContainer.getBoundingClientRect().top + 500,
+    });
+}
+window.addEventListener("load", () => loadImagesByArray(gallery));
+
+/******************************************* RENDER IMAGES ********************************************/
